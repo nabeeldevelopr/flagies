3208ae89ff296bc5f60c826f1200b711def42ee7
nabeeldevelopr <rehaanahmad481@gmail.com>
2025-01-13 11:20:17 +0500

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,8 @@
+let color = prompt("What color you wearing");
+
+if(color==="yellow");{
+    console.log("You are wearingyellow , Please enter and have fun")
+}
+else{
+    console.log("Hina color theme is yellow")
+}
